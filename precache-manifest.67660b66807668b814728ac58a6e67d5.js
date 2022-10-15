33f3b4edcfda863db96900da2b818534d36aaa7a
nikhil <nikhilmane187@gmail.com>
2022-10-15 09:27:44 +0530

    Updates

---
+++ precache-manifest.67660b66807668b814728ac58a6e67d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "63c8d050650b14120835",
+    "url": "/devportfolio/static/css/main.76aa1af7.chunk.css"
+  },
+  {
+    "revision": "63c8d050650b14120835",
+    "url": "/devportfolio/static/js/main.63c8d050.chunk.js"
+  },
+  {
+    "revision": "662a448855c5de048ffc",
+    "url": "/devportfolio/static/js/1.662a4488.chunk.js"
+  },
+  {
+    "revision": "4bae200ecd63e70ceea4",
+    "url": "/devportfolio/static/js/runtime~main.4bae200e.js"
+  },
+  {
+    "revision": "7d1fe24b3072b92a2bd11f8a5d569f9b",
+    "url": "/devportfolio/static/media/me-about.7d1fe24b.jpg"
+  },
+  {
+    "revision": "8d278ea3fddd6336d27842e3e44d51cf",
+    "url": "/devportfolio/static/media/cv.8d278ea3.pdf"
+  },
+  {
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "/devportfolio/static/media/me.0a8cf10e.png"
+  },
+  {
+    "revision": "72a2add4e60babaa64cab037fd2228d7",
+    "url": "/devportfolio/index.html"
+  }
+];
